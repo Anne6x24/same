37f78c29e3b1a3927f381211ae28c9b21b4c5855
“Anne6x24” <Anne6x24@stud.kea.dk>
2022-03-03 14:30:19 +0100

    farver

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      /*baggrund*/
+      primary: "#F4F5EB",
+      /*rød*/
+      secondary: "#B64421",
+      /*sort*/
+      third: "#00000",
+      /*prik lys*/
+      fourth: "#CBB17F",
+      /*prik mørk*/
+      fifth: "#CA8E35",
+      /*brun*/
+      sixth: "#4B352A",
+      /*grøn*/
+      seventh: "#72A287",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
